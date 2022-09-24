9d63acddd5228cbd9ec581c121e39ba28711e16a
balsac7 <70721744+balsac7@users.noreply.github.com>
2022-09-23 23:03:21 -0400

    added meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,11 @@
 import Nav from '../components/Nav'
+import Meta from '../components/Meta'
 import styles from '../styles/Layout.module.css'
 
 const Layout = ({children}) => {
   return (
     <>
-      
+      <Meta />
       <div className={styles.container}>
         <main className={styles.main}>
           {children}
