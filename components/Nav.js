5cc34d86545ce11078a412371c93a3ede1c008af
balsac7 <70721744+balsac7@users.noreply.github.com>
2022-09-24 21:57:39 -0400

    finally fixed nav bar by using divs instead of nav and ul-li

--- components/Nav.js
+++ components/Nav.js
@@ -6,16 +6,16 @@ import navStyles from '../styles/Nav.module.css'
 const Nav = () => {
   return (
     <div className={navStyles.navContainer}>
-      <nav className={navStyles.nav}>
-        <ul>
+      <div className={navStyles.nav}>
+        <div className={navStyles.navStuff}>
           <div className={navStyles.circleContainer}>
             <span className={navStyles.circle}></span>
           </div>
-          <li><Link href={'/'}>DOM</Link></li>
-          <li> / </li>
-          <li><a href={'/projects'}>Projects <FontAwesomeIcon icon={faChevronDown}></FontAwesomeIcon> </a></li>
-        </ul>
-      </nav>
+          <div className={navStyles.list}><Link href={'/'}>DOM</Link></div>
+          <div className={navStyles.list}> / </div>
+          <div className={navStyles.list}><a href={'/projects'}>Projects <FontAwesomeIcon icon={faChevronDown}></FontAwesomeIcon> </a></div>
+        </div>
+      </div>
     </div>
     
   )
