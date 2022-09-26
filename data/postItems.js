02652ad1f009060bdbdc6ac230168c8c7700989c
balsac7 <70721744+balsac7@users.noreply.github.com>
2022-09-26 16:44:10 -0400

    prepping api, blog posts, and pages for notes section

---
+++ data/postItems.js
@@ -0,0 +1,26 @@
+export const postItems = [
+  {
+    id: '1',
+    title: 'Made a portfolio site with Next.JS',
+    excerpt: 'date of post (remember to change)',
+    body: [
+      {
+        header: 'I finally made a portfolio site',
+        subBody: `For a long time, I have thought about creating a portfolio website. Now I have finally done it, and I couldn\'t be happier with it. This website was a great challange for me to not only learn the Next.JS framework and further my understanding of React, but to overhaul my CSS skills. Normally, I would use Bootstrap as I am not familiar with CSS, however I feel that relying on technologies like Bootstrap are counterintuitive and only hold yourself back creatively and technically. This website was based off of  and was built entirely with CSS and by \'eyeballing\' this website. I did not look at the source code for this website and instead decided to put my CSS knowledge to the test by attempting to recreate it`
+      },
+      {
+        header: 'Thoughts on Next.JS',
+        subBody: 'So far, I really like Next.JS simplicity. Specifically how it handles routing, pages, and styles. This saves a lot of time by allowing you to focus on making components without spending 45 minutes reading the React-Router documentation to figure out the frequent syntax changes. Moreover, I love the built in API functionality that is akin to NodeJS and express. This makes for a very lightweight application that can still interact with complex REST API calls and an external database. '
+      },
+      {
+        header: 'Future directions',
+        body: 'I think I would like to implement Next.JS in a full stack application I am working on with NodeJS and MongoDB. Although the provided API functionality with Next is cool, I would still like to keep all API calls exclusive to a backend and keep the front end seperated. Because the projects I make are for educational purposes, I\'m not necessarily concerned about performance right now. My main priority is to learn as much as possible while buiding these web-apps.'
+      }
+    ],
+    link: [
+      {
+        url: 'https://uzu.works/'
+      }
+    ]
+  }
+]
