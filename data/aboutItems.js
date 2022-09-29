d291a83e07d7b9c78aa01c4f1327612935390d26
balsac7 <70721744+balsac7@users.noreply.github.com>
2022-09-28 23:18:54 -0400

    minor fixes

--- data/aboutItems.js
+++ data/aboutItems.js
@@ -2,19 +2,19 @@ export const aboutItems = [
   {
     title: "Projects",
     link: "/projects",
-    bio: "Here are some of my Projects I have worked on",
+    bio: "Contains a couple of the projects I have worked on",
     external: false
   }, 
   {
     title: "LinkedIn",
     link: "https://www.linkedin.com/in/dominick-founds-726a161a4/",
-    bio: "My LinkedIn profile",
+    bio: "My LinkedIn profile where you can view my resume and previous work experience.",
     external: true
   },
   {
     title: "Github",
     link: "https://github.com/DomJF45",
-    bio: "My GitHub where I store lots of code",
+    bio: "My GitHub where I store my code. Here you can find practice, as well as code for some of my projects.",
     external: true
   }
 ]
